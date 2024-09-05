3fbb4666579a6dbb9767b94100e1782ce7387b0f
jpelehwany <jpelehwany@loyola.edu>
2024-09-05 13:14:43 -0400

    added Laure and 5 things about her

---
+++ Lab0.py
@@ -0,0 +1,7 @@
+print("CS151 Lab 0")
+print("Laure")
+print("Laure is 18")
+print ("Laure has 4 pets")
+print("Laure likes the color green")
+print("Laure is current in a computer science class")
+print("Laure has brown hair")
