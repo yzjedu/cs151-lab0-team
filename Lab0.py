dd1447aa904a68db1995cd1f4c9743ca06b0d3e9
LaurePatera <ampatera@loyola.edu>
2024-09-12 08:38:28 -0400

    Tokenization

--- Lab0.py
+++ Lab0.py
@@ -1,3 +1,11 @@
+# Programmers:  Laure Patera and John Elewhany
+# Course:  CS151, Dr. Yalew
+# Due Date: 9/12/24
+# Lab Assignment: 0
+# Problem Statement:  Output information about the programmers
+# Data In: None, there is no user input in this program
+# Data Out:  The names of the programmers and five statements about each of them
+# Credits: Code based on framework from the class homework
 print("CS151 Lab 0")
 print("Laure")
 print("Laure is 18")
@@ -5,3 +13,9 @@ print ("Laure has 4 pets")
 print("Laure likes the color green")
 print("Laure is current in a computer science class")
 print("Laure has brown hair")
+print("John")
+print("John has brown eyes")
+print("John has three dogs")
+print("John enjoys hiking")
+print("John is taking a computer science class")
+print("John is trying his best")
